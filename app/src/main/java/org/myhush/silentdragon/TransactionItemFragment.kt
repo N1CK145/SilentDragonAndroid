3df70f7f44cb9058eae43de1ec80b887e49719f4
Denio <dennis.netterdon@me.com>
2019-11-07 09:57:39 +0100

    make white, whiter, change colour for maintab

--- app/src/main/java/org/myhush/silentdragon/TransactionItemFragment.kt
+++ app/src/main/java/org/myhush/silentdragon/TransactionItemFragment.kt
@@ -77,9 +77,9 @@ class TransactionItemFragment : Fragment() {
                 R.color.colorAccent
             ))
         } else {
-            col.setImageResource(R.color.white_unselected)
+            col.setImageResource(R.color.white_selected)
             amt.setTextColor(ContextCompat.getColor(view.context,
-                R.color.white_unselected
+                R.color.white_selected
             ))
         }
 
