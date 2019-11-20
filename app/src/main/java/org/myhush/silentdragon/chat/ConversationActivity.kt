b6eb5fbf44e5420235a29f7999f9423fec83b6a3
N1CK145 <nickas.gehlen@gmail.com>
2019-11-20 23:20:53 +0100

    Remove actionbar in conversation

--- app/src/main/java/org/myhush/silentdragon/chat/ConversationActivity.kt
+++ app/src/main/java/org/myhush/silentdragon/chat/ConversationActivity.kt
@@ -1,8 +1,12 @@
 package org.myhush.silentdragon.chat
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.FragmentTransaction
+import android.util.AttributeSet
+import android.view.View
+import android.view.Window
 import android.widget.TextView
 import org.myhush.silentdragon.R
 import org.myhush.silentdragon.conversation_item_recive
@@ -17,13 +21,18 @@ class ConversationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_conversation)
         findViewById<TextView>(R.id.textViewContactName2)
 
-        //restoreChat()
+        restoreChat()
+    }
+
+    override fun onCreateView(name: String?, context: Context?, attrs: AttributeSet?): View? {
+        return super.onCreateView(name, context, attrs)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
     }
 
     private fun restoreChat(){
         addMessage("Lorem Ipsum", true)
         addMessage("Lorem Ipsum", true)
-        addMessage("Lorem Ipsum", true)
+        addMessage("Lorem Ipsum", false)
         addMessage("Lorem Ipsum", true)
     }
 
