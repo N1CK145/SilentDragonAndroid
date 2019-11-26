a160d8a33bd3edaf91db1efd6674958ebba50216
N1CK145 <nickas.gehlen@gmail.com>
2019-11-26 18:15:42 +0100

    Send button send message

--- app/src/main/java/org/myhush/silentdragon/chat/ConversationActivity.kt
+++ app/src/main/java/org/myhush/silentdragon/chat/ConversationActivity.kt
@@ -1,13 +1,12 @@
 package org.myhush.silentdragon.chat
 
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.FragmentTransaction
-import android.util.AttributeSet
-import android.view.View
+import android.widget.EditText
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.android.synthetic.main.activity_conversation.*
+import org.myhush.silentdragon.DataModel
 import org.myhush.silentdragon.R
 import org.myhush.silentdragon.conversation_item_recive
 import org.myhush.silentdragon.conversation_item_send
@@ -23,8 +22,24 @@ class ConversationActivity : AppCompatActivity() {
         displayName = intent.extras.getString("displayName")
         findViewById<TextView>(R.id.textViewContactName2).text = displayName
 
+        buttonSend.setOnClickListener{
+            sendMessage()
+        }
 
+
+        contact = Addressbook.contactList[0] // FOR TESTING!!!
         restoreChat()
+    }
+
+    private fun sendMessage(){
+        // FOR TESTING!!!
+        val memo = findViewById<EditText>(R.id.userInput).text.toString()
+        findViewById<EditText>(R.id.userInput).text.clear()
+        var tx = DataModel.TransactionItem("send", 99, "0.00", memo, contact!!.addressList[0], "asdasdasd", 0)
+        val m = Message("addr", tx)
+        attachMessage(m)
+        m.messageType = MessageType.RECIEVE
+        attachMessage(m)
     }
 
     private fun restoreChat() {
